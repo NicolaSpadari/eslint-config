52718ac413f81c33cfde4b7460d5e4eed28153da
Nicola Spadari <spadari.nicola@gmail.com>
2022-01-17 17:48:40 +0100

    Remove typescript

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,13 +3,9 @@ module.exports = {
 		{
 			files: ["*.vue"],
 			parser: "vue-eslint-parser",
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-			},
 			rules: {
 				"no-unused-vars": "off",
 				"no-undef": "off",
-				"@typescript-eslint/no-unused-vars": "off",
 			},
 		},
 	],
