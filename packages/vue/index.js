d528c43642f0ad9318918650932ff13ae68053b9
Nicola Spadari <spadari.nicola@gmail.com>
2022-01-17 19:01:55 +0100

    Updates

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,13 +3,17 @@ module.exports = {
 		{
 			files: ["*.vue"],
 			parser: "vue-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
 			rules: {
 				"no-unused-vars": "off",
 				"no-undef": "off",
+				"@typescript-eslint/no-unused-vars": "off",
 			},
 		},
 	],
-	extends: ["plugin:vue/vue3-recommended"],
+	extends: ["plugin:vue/vue3-recommended", "@spada/eslint-config-ts"],
 	rules: {
 		"vue/max-attributes-per-line": "off",
 		"vue/no-v-html": "off",
